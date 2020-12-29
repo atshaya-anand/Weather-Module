72bcd023bd981a01fc4bdbd93a0685140a144b5d
Atshaya <atshaya1699@gmail.com>
2020-12-29 12:09:41 +0530

    added changes

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ const weather = require('./node-weather.js');
 var city = "%65san diego!!    ";
 
 console.log(`Today's Weather for `+city+` is ----- `);
-weather.getWeather(city);
+var det = weather.getWeather(city);
+console.log('weather',det);
--- node-weather.js
+++ node-weather.js
@@ -4,7 +4,7 @@ var weather = '';
 var error = '';
 
 
-exports.getWeather = function(loc){
+exports.getWeather = async function(loc){
     
     loc = loc.trim();
     var city = loc.replace(/[^a-zA-Z ]/g, "");
@@ -25,22 +25,31 @@ exports.getWeather = function(loc){
     }
 
     var url = `https://www.weather-forecast.com/locations/`+city+`/forecasts/latest`;
-    request(url, function (err, response, body) {
-        
-        if(err){
-          console.log('error:', error);
-        } else {
-          var pageArray = body.split('(1&ndash;3 days):</div><p class="location-summary__text"><span class="phrase">');
-          var secondPageArray = pageArray[1].split('</span></p></div><div class="location-summary__item location-summary__item--js is-truncated"><div class="location-summary__heading-with-ext"><h2 class="location-summary__heading">');
-          weather = secondPageArray[0];
-          console.log(weather);
-        }
-
-      });
+     
+    weather = await sendRequest(url).then(console.log('hey'));
+    return weather;
     
-    //console.log(weather);
-    //return weather;
-
 }
 
+function sendRequest(url){
+
+  return new Promise((resolve,reject) =>{
+    request(url, function (err, response, body)  {
+          
+      if(err) {
+        console.log('error:', err);
+        reject(err);
+      } else {
+        var pageArray = body.split('(1&ndash;3 days):</div><p class="location-summary__text"><span class="phrase">');
+        var secondPageArray = pageArray[1].split('</span></p></div><div class="location-summary__item location-summary__item--js is-truncated"><div class="location-summary__heading-with-ext"><h2 class="location-summary__heading">');
+        //console.log("inside",secondPageArray[0]);
+        resolve(secondPageArray[0]);
+      }
+  
+    });
+  });
+
+  
+}
+  
 
