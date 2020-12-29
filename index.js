649babb0a86ef1b385caef71b6c823199bee6d4f
Atshaya <atshaya1699@gmail.com>
2020-12-29 20:19:53 +0530

    modified all changes

--- index.js
+++ index.js
@@ -1,8 +1,45 @@
-const weather = require('./node-weather.js');
+const request = require('request');
 
-var city = "%65san diego!!    ";
+var weather = '';
 
-console.log(`Today's Weather for `+city+` is ----- `);
-weather.getWeather(city).then(response =>{
-    console.log('weather',response);
-});
+exports.getWeather = async function(loc){
+    
+    loc = loc.trim();
+    var city = loc.replace(/[^a-zA-Z ]/g, "");
+    var reqCity = city.split(" ");
+    
+    if (reqCity.length > 1) {
+      for(var i=0;i<reqCity.length;i++) {
+        reqCity[i] = reqCity[i].charAt(0).toUpperCase() + reqCity[i].slice(1);
+      }
+      city = reqCity.join();
+      city = city.replace(",","-");
+    } else {
+      city = city.charAt(0).toUpperCase() + city.slice(1);
+    }
+
+    var url = `https://www.weather-forecast.com/locations/`+city+`/forecasts/latest`;
+     
+    weather = await sendRequest(url);
+    return weather;
+    
+}
+
+function sendRequest(url){
+
+  return new Promise((resolve,reject) =>{
+    request(url, function (err, response, body)  {
+          
+      if(err) {
+        console.log('error:', err);
+        reject(err);
+      } else {
+        var pageArray = body.split('(1&ndash;3 days):</div><p class="location-summary__text"><span class="phrase">');
+        var secondPageArray = pageArray[1].split('</span></p></div><div class="location-summary__item location-summary__item--js is-truncated"><div class="location-summary__heading-with-ext"><h2 class="location-summary__heading">');
+        resolve(secondPageArray[0]);
+      }
+  
+    });
+  });
+
+}
